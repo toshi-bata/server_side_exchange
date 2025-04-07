553f5d9d9fca75cf77c74da52ddf1c0f7776e11d
Toshiaki Kawabata <toshiaki@techsoft3d.com>
2025-04-07 12:52:42 +0900

    Ported 2025.2.0

--- ExServer/VS2019_ExServer.bat
+++ ExServer/VS2019_ExServer.bat
@@ -1,5 +1,5 @@
 SET LIBMICRO_INSTALL_DIR=C:\SDK\libmicrohttpd\libmicrohttpd-1.0.0-w32-bin
-SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2025.1.0
-SET HCOMMUNICATOR_INSTALL_DIR=C:\SDK\Communicator\HOOPS_Communicator_2025.1.0
+SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2025.2.0
+SET HCOMMUNICATOR_INSTALL_DIR=C:\SDK\Communicator\HOOPS_Communicator_2025.2.0
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" ExServer.sln
